f3ffcef47d8a49d733e9bfc52925e1302c57621d
Ward Nakchbandi (Cosmic Fusion) <hotrod.master@hotmail.com>
2023-08-08 19:00:32 +0300

    update control

--- main.sh
+++ main.sh
@@ -5,6 +5,7 @@ set -e
 git clone https://gitlab.freedesktop.org/mesa/mesa
 cp -rvf ./debian ./mesa/
 cd ./mesa
+sed -i ' 1 s/.*/& - PikaOS YellowBirb Vulkan Only Mesa /' ./VERSION
 touch debian/changelog
 echo -e "mesa (23.3.git."$(git rev-parse --short HEAD)"-99pika"$(date '+%Y%m%d')") lunar; urgency=medium\n\n  * New GIT Release\n\n -- Ward Nakchbandi <hotrod.master@hotmail.com> Sat, 01 Oct 2022 14:50:00 +0200" > debian/changelog
 
