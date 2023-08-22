2c994dfbc188f5f4d83f28c78a9843ec579c298c
ferrreo <harderthanfire@gmail.com>
2023-08-22 16:52:39 +0100

    Update main32.sh

--- main32.sh
+++ main32.sh
@@ -5,9 +5,10 @@ set -e
 git clone https://gitlab.freedesktop.org/mesa/mesa ./mesa-git
 cp -rvf ./debian ./mesa-git/
 cd ./mesa-git
+git submodule update --init
 sed -i ' 1 s/.*/& - PikaOS YellowBirb Mesa Git /' ./VERSION
 touch debian/changelog
-echo -e "mesa-git (23.3-99pika"$(date '+%Y%m%d')".git.6."$(git rev-parse --short HEAD)") lunar; urgency=medium\n\n  * New GIT Release\n\n -- Ward Nakchbandi <hotrod.master@hotmail.com> Sat, 01 Oct 2022 14:50:00 +0200" > debian/changelog
+echo -e "mesa-git (23.3-99pika"$(date '+%Y%m%d')".git.7."$(git rev-parse --short HEAD)") lunar; urgency=medium\n\n  * New GIT Release\n\n -- Ward Nakchbandi <hotrod.master@hotmail.com> Sat, 01 Oct 2022 14:50:00 +0200" > debian/changelog
 
 # Get build deps
 apt-get build-dep ./ -y
